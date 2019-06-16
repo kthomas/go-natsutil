b2c69d97ef2c68a3aff65062ceee4af2c63ba3c7
Kyle Thomas <kyle@provide.services>
2019-06-16 07:13:21 -0400

    Add robust NATS and NATS streaming connection factories

--- env.go
+++ env.go
@@ -15,63 +15,68 @@ const defaultNatsDeadLetterSubject = "nats.deadletter"
 var (
 	log *logger.Logger
 
-	natsConnection          *nats.Conn
-	natsStreamingConnection *stan.Conn
+	natsConnections          map[string]*nats.Conn
+	natsStreamingConnections map[string]*stan.Conn
+
+	natsConnectionMutex          sync.Mutex
+	natsStreamingConnectionMutex sync.Mutex
 
 	natsClientPrefix        string
 	natsClusterID           string
-	natsConsumerConcurrency uint64
+	natsConsumerConcurrency uint64 // number of subscriptions to open per NATS connection
 	natsDeadLetterSubject   string
 	natsToken               string
 	natsURL                 string
 	natsStreamingURL        string
-
-	bootstrapOnce sync.Once
 )
 
 func init() {
-	bootstrapOnce.Do(func() {
-		lvl := os.Getenv("LOG_LEVEL")
+	lvl := os.Getenv("NATS_LOG_LEVEL")
+	if lvl == "" {
+		lvl = os.Getenv("LOG_LEVEL")
 		if lvl == "" {
 			lvl = "INFO"
 		}
-		log = logger.NewLogger("go-natsutil", lvl, true)
+	}
+	log = logger.NewLogger("go-natsutil", lvl, true)
 
-		if os.Getenv("NATS_TOKEN") != "" {
-			natsToken = os.Getenv("NATS_TOKEN")
-		}
+	if os.Getenv("NATS_TOKEN") != "" {
+		natsToken = os.Getenv("NATS_TOKEN")
+	}
 
-		if os.Getenv("NATS_URL") != "" {
-			natsURL = os.Getenv("NATS_URL")
-		}
+	if os.Getenv("NATS_URL") != "" {
+		natsURL = os.Getenv("NATS_URL")
+	}
 
-		if os.Getenv("NATS_CLIENT_PREFIX") != "" {
-			natsClientPrefix = os.Getenv("NATS_CLIENT_PREFIX")
-		} else {
-			natsClientPrefix = "go-natsutil"
-		}
+	if os.Getenv("NATS_CLIENT_PREFIX") != "" {
+		natsClientPrefix = os.Getenv("NATS_CLIENT_PREFIX")
+	} else {
+		natsClientPrefix = "go-natsutil"
+	}
 
-		if os.Getenv("NATS_CLUSTER_ID") != "" {
-			natsClusterID = os.Getenv("NATS_CLUSTER_ID")
-		}
+	if os.Getenv("NATS_CLUSTER_ID") != "" {
+		natsClusterID = os.Getenv("NATS_CLUSTER_ID")
+	}
 
-		if os.Getenv("NATS_DEAD_LETTER_SUBJECT") != "" {
-			natsDeadLetterSubject = os.Getenv("NATS_DEAD_LETTER_SUBJECT")
-		} else {
-			natsDeadLetterSubject = defaultNatsDeadLetterSubject
-		}
+	if os.Getenv("NATS_DEAD_LETTER_SUBJECT") != "" {
+		natsDeadLetterSubject = os.Getenv("NATS_DEAD_LETTER_SUBJECT")
+	} else {
+		natsDeadLetterSubject = defaultNatsDeadLetterSubject
+	}
 
-		if os.Getenv("NATS_STREAMING_URL") != "" {
-			natsStreamingURL = os.Getenv("NATS_STREAMING_URL")
+	if os.Getenv("NATS_STREAMING_URL") != "" {
+		natsStreamingURL = os.Getenv("NATS_STREAMING_URL")
 
-			if os.Getenv("NATS_STREAMING_CONCURRENCY") != "" {
-				concurrency, err := strconv.ParseUint(os.Getenv("NATS_STREAMING_CONCURRENCY"), 10, 8)
-				if err == nil {
-					natsConsumerConcurrency = concurrency
-				} else {
-					natsConsumerConcurrency = 1
-				}
+		if os.Getenv("NATS_STREAMING_CONCURRENCY") != "" {
+			concurrency, err := strconv.ParseUint(os.Getenv("NATS_STREAMING_CONCURRENCY"), 10, 8)
+			if err == nil {
+				natsConsumerConcurrency = concurrency
+			} else {
+				natsConsumerConcurrency = 1
 			}
 		}
-	})
+	}
+
+	natsConnections = map[string]*nats.Conn{}
+	natsStreamingConnections = map[string]*stan.Conn{}
 }
--- nats.go
+++ nats.go
@@ -2,6 +2,7 @@ package natsutil
 
 import (
 	"fmt"
+	"time"
 
 	uuid "github.com/kthomas/go.uuid"
 	nats "github.com/nats-io/nats.go"
@@ -14,58 +15,113 @@ func GetNatsConsumerConcurrency() uint64 {
 	return natsConsumerConcurrency
 }
 
-// GetNatsConnection returns the cached nats connection or establishes
+// GetNatsConnection returns the cached NATS connection or establishes
 // and caches it if it doesn't exist
-func GetNatsConnection() *nats.Conn {
-	if natsConnection == nil || natsConnection.IsClosed() {
-		conn, err := nats.Connect(natsURL, nats.Token(natsToken))
-		if err == nil {
-			natsConnection = conn
-		} else {
-			log.Warningf("NATS connection failed; %s", err.Error())
-		}
+func GetNatsConnection(drainTimeout time.Duration) (conn *nats.Conn, err error) {
+	clientID, err := uuid.NewV4()
+	if err != nil {
+		log.Warningf("Failed to generate client id for NATS connection; %s", err.Error())
+		return nil, err
 	}
 
-	return natsConnection
+	conn, err = nats.Connect(natsURL,
+		nats.Name(fmt.Sprintf("%s-%s", natsClientPrefix, clientID.String())),
+		nats.Token(natsToken),
+		nats.Secure(), // FIXME-- do not use Secure() by itself; see RootCAs() or pass a proper TLS config into Secure()
+		nats.MaxReconnects(-1),
+		nats.ReconnectBufSize(-1),
+		nats.DrainTimeout(drainTimeout),
+		// nats.AuthTokenHandler(func() string { })
+		nats.ClosedHandler(func(_conn *nats.Conn) {
+			log.Debugf("NATS connection %s closed", _conn.Opts.Name)
+			if _conn != nil {
+				lastErr := _conn.LastError()
+				if lastErr != nil {
+					log.Warningf("NATS connection %s closed; %s", _conn.Opts.Name, lastErr.Error())
+				}
+			}
+		}),
+		nats.DisconnectErrHandler(func(_conn *nats.Conn, _err error) {
+			log.Debugf("NATS connection %s disconnected", _conn.Opts.Name)
+			natsConnectionMutex.Lock()
+			delete(natsConnections, _conn.Opts.Name)
+			natsConnectionMutex.Unlock()
+		}),
+		nats.ReconnectHandler(func(_conn *nats.Conn) {
+			log.Debugf("NATS connection reestablished; %s", _conn.Opts.Name)
+		}),
+		nats.DiscoveredServersHandler(func(_conn *nats.Conn) {
+			log.Debugf("NATS connection discovered peers; %s", _conn.Opts.Name)
+		}),
+	)
+
+	if err != nil {
+		log.Warningf("NATS connection failed; %s", err.Error())
+		return nil, err
+	} else {
+		clientID, err := conn.GetClientID()
+		if err != nil {
+			log.Warningf("Failed to retrieve valid client id from NATS connection; %s", err.Error())
+			return nil, err
+		}
+
+		natsConnectionMutex.Lock()
+		defer natsConnectionMutex.Unlock()
+		natsConnections[conn.Opts.Name] = conn
+
+		log.Debugf("Caching NATS connection for client id: %s", clientID)
+	}
+
+	return conn, nil
 }
 
-// GetNatsStreamingConnection returns the cached nats streaming connection or
+// GetNatsStreamingConnection returns the cached NATS streaming connection or
 // establishes and caches it if it doesn't exist
-func GetNatsStreamingConnection(connectionLostHandler func(_ stan.Conn, reason error)) *stan.Conn {
-	if natsStreamingConnection == nil {
-		clientID, err := uuid.NewV4()
-		if err != nil {
-			log.Warningf("Failed to generate client id for NATS streaming connection; %s", err.Error())
-			return nil
-		}
+func GetNatsStreamingConnection(drainTimeout time.Duration, connectionLostHandler func(_ stan.Conn, reason error)) (sconn stan.Conn, err error) {
+	natsStreamingConnectionMutex.Lock()
+	defer natsStreamingConnectionMutex.Unlock()
 
-		natsConn, err := nats.Connect(natsStreamingURL, nats.Token(natsToken))
-		if err != nil {
-			log.Warningf("NATS connection failed; %s", err.Error())
-			return nil
-		}
+	conn, err := GetNatsConnection(drainTimeout)
+	if err != nil {
+		log.Warningf("NATS connection failed; %s", err.Error())
+		return nil, err
+	}
 
-		conn, err := stan.Connect(natsClusterID, fmt.Sprintf("%s-%s", natsClientPrefix, clientID.String()), stan.NatsConn(natsConn), stan.SetConnectionLostHandler(func(c stan.Conn, reason error) {
-			natsStreamingConnection = nil
+	clientName := []byte(conn.Opts.Name)
+	sClientID := fmt.Sprintf("%s-%s", natsClientPrefix, conn.Opts.Name)
+	sconn, err = stan.Connect(natsClusterID,
+		sClientID,
+		stan.NatsConn(conn),
+		stan.SetConnectionLostHandler(func(c stan.Conn, reason error) {
+			log.Warningf("NATS streaming connection lost: %s; %s", c, reason.Error())
+			_conn := c.NatsConn()
+			_name := string(clientName)
+			if _conn != nil && _conn.Opts.Name == _name {
+				if staleConn, staleConnOk := natsConnections[_name]; staleConnOk {
+					natsConnectionMutex.Lock()
+					delete(natsConnections, _name)
+					natsConnectionMutex.Unlock()
+
+					if !staleConn.IsClosed() {
+						log.Debugf("Attempting to drain NATS connection: %s", clientName)
+						err = staleConn.Drain()
+						if err != nil {
+							log.Warningf("Failed to drain stale NATS connection: %s; %s", clientName, err.Error())
+						}
+					}
+				}
+			}
+
 			if connectionLostHandler != nil {
 				connectionLostHandler(c, reason)
 			}
 		}))
-		if err == nil {
-			natsStreamingConnection = &conn
-		} else {
-			log.Warningf("NATS streaming connection failed; %s", err.Error())
-		}
-	} else {
-		conn := *natsStreamingConnection
-		if conn.NatsConn() == nil || conn.NatsConn().IsClosed() {
-			log.Debugf("NATS streaming closed; attempting to reconnect: %s", natsStreamingURL)
-			natsStreamingConnection = nil
-			return GetNatsStreamingConnection(connectionLostHandler)
-		}
+	if err != nil {
+		log.Warningf("NATS streaming connection failed; %s", err.Error())
+		return nil, err
 	}
 
-	return natsStreamingConnection
+	return sconn, nil
 }
 
 // Nack the given NATS message
