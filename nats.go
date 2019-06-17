f6386a6997910eae1905090ea0adea17b4cce567
Kyle Thomas <kyle@provide.services>
2019-06-17 04:21:01 -0400

    Extend NATS options when TLS is being forced

--- nats.go
+++ nats.go
@@ -56,6 +56,7 @@ func GetNatsConnection(drainTimeout time.Duration) (conn *nats.Conn, err error) 
 	}
 
 	if natsForceTLS {
+		options = options[0:len(options)]
 		options[len(options)] = nats.Secure()
 	} else {
 		// certificates := make([]tls.Certificate, 0)
