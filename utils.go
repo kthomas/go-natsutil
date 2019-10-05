c26ab647994ea03915531e3c8b60e4ac9a6d21a5
Kyle Thomas <kyle@provide.services>
2019-10-05 01:29:05 -0400

    Use shared NATS streaming connection by default in convenience wrapper

--- utils.go
+++ utils.go
@@ -1,15 +1,21 @@
 package natsutil
 
 import (
+	"fmt"
 	"sync"
+	"time"
 
 	stan "github.com/nats-io/stan.go"
 )
 
 var sharedNatsStreamingConnectionMutex sync.Mutex
 
-// EstablishSharedNATSStreamingConnection establishes (if conn is nil) or reestablishes the given NATS streaming connection
-func EstablishSharedNATSStreamingConnection() error {
+// EstablishSharedNatsStreamingConnection establishes or reestablishes the default shared NATS streaming connection
+func EstablishSharedNatsStreamingConnection() error {
+	if IsSharedNatsStreamingConnectionValid() {
+		return nil
+	}
+
 	sharedNatsStreamingConnectionMutex.Lock()
 	defer sharedNatsStreamingConnectionMutex.Unlock()
 
@@ -22,7 +28,7 @@ func EstablishSharedNATSStreamingConnection() error {
 	return nil
 }
 
-// GetSharedNatsStreamingConnection retrieves the NATS streaming connection
+// GetSharedNatsStreamingConnection retrieves the default shared NATS streaming connection
 func GetSharedNatsStreamingConnection() (stan.Conn, error) {
 	if sharedNatsStreamingConnection != nil {
 		conn := sharedNatsStreamingConnection.NatsConn()
@@ -31,7 +37,7 @@ func GetSharedNatsStreamingConnection() (stan.Conn, error) {
 		}
 	}
 
-	err := EstablishSharedNATSStreamingConnection()
+	err := EstablishSharedNatsStreamingConnection()
 	if err != nil {
 		log.Warningf("Failed to establish shared NATS streaming connection; %s", err.Error())
 		return sharedNatsStreamingConnection, err
@@ -39,8 +45,8 @@ func GetSharedNatsStreamingConnection() (stan.Conn, error) {
 	return sharedNatsStreamingConnection, nil
 }
 
-// NATSPublish publishes a NATS message using the configured NATS streaming environment
-func NATSPublish(subject string, msg []byte) error {
+// NatsPublish publishes a NATS message using the default shared NATS streaming environment
+func NatsPublish(subject string, msg []byte) error {
 	natsConnection, err := GetSharedNatsStreamingConnection()
 	if err != nil {
 		log.Warningf("Failed to retrieve shared NATS streaming connection for publish; %s", err.Error())
@@ -49,8 +55,8 @@ func NATSPublish(subject string, msg []byte) error {
 	return natsConnection.Publish(subject, msg)
 }
 
-// NATSPublishAsync asynchronously publishes a NATS message using the configured NATS streaming environment
-func NATSPublishAsync(subject string, msg []byte) (*string, error) {
+// NatsPublishAsync asynchronously publishes a NATS message using the default shared NATS streaming connection
+func NatsPublishAsync(subject string, msg []byte) (*string, error) {
 	natsConnection, err := GetSharedNatsStreamingConnection()
 	if err != nil {
 		log.Warningf("Failed to retrieve shared NATS streaming connection for asynchronous publish; %s", err.Error())
@@ -66,6 +72,54 @@ func NATSPublishAsync(subject string, msg []byte) (*string, error) {
 		return nil, err
 	}
 	return stringOrNil(guid), err
+}
+
+// AttemptNack tries to Nack the given NATS message if it meets basic time-based deadlettering criteria, using the default shared NATS streaming connection
+func AttemptNack(msg *stan.Msg, timeout int64) {
+	timeoutMillis := timeout / 1000 / 1000
+	if ShouldDeadletter(msg, timeoutMillis) {
+		log.Debugf("Nacking redelivered %d-byte message after %dms timeout: %s", msg.Size(), timeoutMillis, msg.Subject)
+		Nack(msg)
+	}
+}
+
+// Nack the given NATS message using the default shared NATS streaming connection
+func Nack(msg *stan.Msg) error {
+	if !IsSharedNatsStreamingConnectionValid() {
+		err := fmt.Errorf("Cannot Nack %d-byte NATS message on subject: %s", msg.Size(), msg.Subject)
+		log.Warning(err.Error())
+		return err
+	}
+	_, err := sharedNatsStreamingConnection.PublishAsync(natsDeadLetterSubject, msg.Data, func(_ string, err error) {
+		if err == nil {
+			err = msg.Ack()
+			if err == nil {
+				log.Debugf("Nacked %d-byte NATS message on subject: %s", msg.Size(), msg.Subject)
+			} else {
+				log.Warningf("Failed to Nack NATS message which was successfully dead-lettered: %s", err.Error())
+			}
+		}
+	})
+	if err != nil {
+		log.Warningf("Failed to Nack %d-byte NATS message on subject: %s; publish failed: %s", msg.Size(), msg.Subject, err.Error())
+	}
+	return err
+}
+
+// ShouldDeadletter determines if a given message should be deadlettered by converting the
+// given message timestamp and deadletterTimeout values from nanosecond and millisecond
+// resolutions, respectively, to seconds and comparing against current UTC time
+func ShouldDeadletter(msg *stan.Msg, deadletterTimeoutMillis int64) bool {
+	return msg.Redelivered && time.Now().UTC().Unix()-(msg.Timestamp/1000/1000/1000) >= (deadletterTimeoutMillis/1000)
+}
+
+// IsSharedNatsStreamingConnectionValid returns true if the default NATS streaming
+func IsSharedNatsStreamingConnectionValid() bool {
+	return !(sharedNatsStreamingConnection == nil ||
+		sharedNatsStreamingConnection.NatsConn() == nil ||
+		sharedNatsStreamingConnection.NatsConn().IsClosed() ||
+		sharedNatsStreamingConnection.NatsConn().IsDraining() ||
+		sharedNatsStreamingConnection.NatsConn().IsReconnecting())
 }
 
 func stringOrNil(str string) *string {
